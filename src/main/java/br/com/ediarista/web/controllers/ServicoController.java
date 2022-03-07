0de79f2432db10901923c467716f6752adeef9c8
jonny.alves <jonny.alves@portoseguro.com.br>
2022-03-07 18:03:37 -0300

    adicionando pagina de cadastro de servico

--- src/main/java/br/com/ediarista/web/controllers/ServicoController.java
+++ src/main/java/br/com/ediarista/web/controllers/ServicoController.java
@@ -1,8 +1,10 @@
 package br.com.ediarista.web.controllers;
 
 import br.com.ediarista.core.models.Servico;
+import br.com.ediarista.core.models.enums.Icone;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
 
@@ -15,7 +17,11 @@ public class ServicoController {
         var modelAndView = new ModelAndView("admin/servico/form");
 
         modelAndView.addObject("servico", new Servico());
-
         return modelAndView;
+    }
+
+    @ModelAttribute("icones")
+    public Icone[] getIcones(){
+        return Icone.values();
     }
 }
