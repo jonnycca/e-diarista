48aa2c9e05bdf4d045091191e302132a53938c73
jonny.alves <jonny.alves@portoseguro.com.br>
2022-03-07 18:59:08 -0300

    criando operação de cadastro de serviço

--- src/main/java/br/com/ediarista/web/controllers/ServicoController.java
+++ src/main/java/br/com/ediarista/web/controllers/ServicoController.java
@@ -2,15 +2,21 @@ package br.com.ediarista.web.controllers;
 
 import br.com.ediarista.core.models.Servico;
 import br.com.ediarista.core.models.enums.Icone;
+import br.com.ediarista.core.models.repositories.ServicoRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
 
 @Controller
 @RequestMapping("/admin/servicos")
 public class ServicoController {
+
+    @Autowired
+    private ServicoRepository repository;
 
     @GetMapping("/cadastrar")
     public ModelAndView cadastrar(){
@@ -23,5 +29,11 @@ public class ServicoController {
     @ModelAttribute("icones")
     public Icone[] getIcones(){
         return Icone.values();
+    }
+
+    @PostMapping("/cadastrar")
+    public String cadastrar(Servico servico){
+        repository.save(servico);
+        return "redirect:/admin/servicos/cadastrar";
     }
 }
